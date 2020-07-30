cfbd32e3205f6d9db9a3e3081ea3a3f62642e85a
oppara <oppara@oppara.tv>
2020-07-30 23:41:40 +0900

    fix #1

--- src/app.js
+++ src/app.js
@@ -3,12 +3,28 @@
 'use strict';
 
 const tweet2hatebu = require('./tweet2hatebu');
+const aws = require('aws-sdk');
+
+const sendSns = message => {
+  const params = {
+    Message: message,
+    Subject: 'ERROR: tweet2hatebu',
+    TopicArn: process.env.TOPIC_ARN,
+  };
+  const sns = new aws.SNS({
+    apiVersion: '2010-03-31',
+    region: 'ap-northeast-1',
+  });
+
+  return sns.publish(params).promise();
+};
 
 exports.handler = async (event, context) => {
   try {
     await tweet2hatebu.tweet2hatebu();
   } catch (err) {
     console.error(err);
+    await sendSns(err.message);
   }
 
   // console.log("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2));
