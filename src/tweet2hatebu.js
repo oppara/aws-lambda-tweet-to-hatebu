dde6ad7ca325cf8fdb47565a688cbb76589f31c5
oppara <oppara@oppara.tv>
2020-08-16 21:54:18 +0900

    Fix 429 Too Many Requests

--- src/tweet2hatebu.js
+++ src/tweet2hatebu.js
@@ -2,16 +2,17 @@
 
 'use strict';
 
-const moment = require('moment');
 const compose = require('request-compose');
 compose.Request.oauth = require('request-oauth');
 const request = compose.client;
 
-const FROM_DATE_HOURS = 12;
-const MAX_TWEETS = 100;
-const TWITTER_API_URL =
-  'https://api.twitter.com/1.1/tweets/search/30day/prod.json';
 const HATENA_API_URL = 'https://bookmark.hatenaapis.com/rest/1/my/bookmark';
+
+// https://developer.twitter.com/en/docs/twitter-api/v1/tweets/timelines/api-reference/get-statuses-user_timeline
+const TWITTER_API_URL =
+  'https://api.twitter.com/1.1/statuses/user_timeline.json';
+const TWEETS_PER_REQUEST = 100;
+const TWITTER_SCREEN_NAME = 'opp_hatena';
 const EXCLUDE_TAGS = ['todo', 'instapaper'];
 
 const post2hatebu = (url, tags) => {
@@ -29,23 +30,19 @@ const post2hatebu = (url, tags) => {
 };
 
 const retrieveTweets = () => {
-  const data = {
-    query: 'from:opp_hatena lang:ja',
-    maxResults: MAX_TWEETS,
-    fromDate: moment()
-      .subtract(FROM_DATE_HOURS, 'hours')
-      .utc()
-      .format('YYYYMMDDHHmm'),
+  const query = {
+    screen_name: TWITTER_SCREEN_NAME,
+    count: TWEETS_PER_REQUEST
   };
 
   return request({
-    method: 'POST',
+    method: 'GET',
     url: TWITTER_API_URL,
     headers: {
       authorization: 'Bearer ' + process.env.TWITTER_BEARER,
       'content-type': 'application/json',
     },
-    body: JSON.stringify(data),
+    qs: query
   });
 };
 
@@ -62,15 +59,14 @@ const excludeTags = (tag) => {
 
 exports.tweet2hatebu = async () => {
   const { body } = await retrieveTweets();
-  const len = body.results.length;
+  const len = body.length;
   for (let i = 0; i < len; i++) {
-    const obj = body.results[i];
+    const obj = body[i];
     const [title, tmp] = obj.text.split(' B! ');
     if (typeof tmp === 'undefined') {
       return;
     }
     const [url, tag] = tmp.split(' tags: ');
-    // const tags = typeof tag !== 'undefined' ? tag.split(', ') : '';
     const tags = excludeTags(tag);
     console.log(title + '\t' + url + '\t' + tags);
     await post2hatebu(url, tags);
